440f22d334ad1ac3c69a7c60335534fa206be921
takumin <takumiiinn@gmail.com>
2022-09-18 13:29:45 +0900

    fix: cleanup github actions cache filepath

--- scripts/cleanup-github-actions-cache.bash
+++ scripts/cleanup-github-actions-cache.bash
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-PROJECT_DIR="$(cd "$(dirname "${SCRIPT_DIR}/..")" && pwd)"
+PROJECT_DIR="$(cd "$(dirname "${SCRIPT_DIR}/../..")" && pwd)"
 
 date '+%s' > "${PROJECT_DIR}/.github/dependency/actions-cache-version"
