ed2116d007b1ef735de6c3c4bb35cacc96d146c4
takumin <takumiiinn@gmail.com>
2022-09-18 15:00:49 +0900

    add rebuild-repository.bash

---
+++ scripts/rebuild-repository.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ ! -x "$(command -v go-imports-rename)" ]; then
+	go install github.com/sirkon/go-imports-rename@latest
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+PROJECT_DIR="$(cd "$(dirname "${SCRIPT_DIR}/../..")" && pwd)"
+
+cd "${PROJECT_DIR}"
+
+GITHUB_NAME_WITH_OWNER="$(gh repo view --json nameWithOwner --jq '.nameWithOwner')"
+GITHUB_OWNER="${GITHUB_NAME_WITH_OWNER%/*}"
+GITHUB_REPOS="${GITHUB_NAME_WITH_OWNER##*/}"
+
+ORIGIN_PKG="github.com/takumin/boilerplate-golang-cli"
+PROJECT_PKG="github.com/${GITHUB_OWNER}/${GITHUB_REPOS}"
+
+go mod edit -module "${PROJECT_NAME}"
+go-imports-rename -s "${ORIGIN_PKG} => ${PROJECT_PKG}"
