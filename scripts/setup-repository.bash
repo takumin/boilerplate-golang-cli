05ab140f5288e980580c173c54553c1e5ac8c937
takumin <takumiiiinn@gmail.com>
2023-08-14 23:10:40 +0900

    feat: Initial implemented metadata packages

--- scripts/setup-repository.bash
+++ scripts/setup-repository.bash
@@ -8,7 +8,7 @@ PROJECT_DIR="$(cd "$(dirname "${SCRIPT_DIR}/../..")" && pwd)"
 cd "${PROJECT_DIR}"
 
 ORIGIN_OWNER="takumin"
-ORIGIN_AUTHOR="Takumi Takahashi"
+ORIGIN_AUTHOR_NAME="Takumi Takahashi"
 ORIGIN_REPOSITORY="boilerplate-golang-cli"
 ORIGIN_DESCRIPTION="Boilerplate Golang CLI Tool"
 
@@ -18,8 +18,8 @@ GITHUB_OWNER="${GITHUB_NAME_WITH_OWNER%/*}"
 GITHUB_REPOSITORY="${GITHUB_NAME_WITH_OWNER##*/}"
 GITHUB_DESCRIPTION="$(gh repo view --json description --jq '.description')"
 
-GITHUB_AUTHOR="$(gh api "users/${GITHUB_OWNER}" --jq '.name')"
-[[ -z "${GITHUB_AUTHOR}" ]] && GITHUB_AUTHOR="${GITHUB_OWNER}"
+GITHUB_AUTHOR_NAME="$(gh api "users/${GITHUB_OWNER}" --jq '.name')"
+[[ -z "${GITHUB_AUTHOR_NAME}" ]] && GITHUB_AUTHOR_NAME="${GITHUB_OWNER}"
 
 ORIGIN_URL="github.com/${ORIGIN_OWNER}/${ORIGIN_REPOSITORY}"
 GITHUB_URL="github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY}"
@@ -27,23 +27,23 @@ GITHUB_URL="github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY}"
 go mod edit -module "${GITHUB_URL}"
 go-imports-rename -s "${ORIGIN_URL} => ${GITHUB_URL}"
 
-sed -i -E "s@Author.*string.*=.*// ###BOILERPLATE_AUTHOR###@Author string = \"${GITHUB_AUTHOR}\"@" main.go
-sed -i -E "s@AppName.*string.*=.*// ###BOILERPLATE_APP_NAME###@AppName string = \"${GITHUB_REPOSITORY}\"@" main.go
-sed -i -E "s@AppDesc.*string.*=.*// ###BOILERPLATE_APP_DESC###@AppDesc string = \"${GITHUB_DESCRIPTION}\"@" main.go
+sed -i -E "s@appName.*string.*=.*// ###BOILERPLATE_APP_NAME###@appName string = \"${GITHUB_REPOSITORY}\"@" internal/metadata/metadata.go
+sed -i -E "s@appDesc.*string.*=.*// ###BOILERPLATE_APP_DESC###@appDesc string = \"${GITHUB_DESCRIPTION}\"@" internal/metadata/metadata.go
+sed -i -E "s@authorName.*string.*=.*// ###BOILERPLATE_AUTHOR_NAME###@authorName string = \"${GITHUB_AUTHOR_NAME}\"@" internal/metadata/metadata.go
 
 gofmt -w .
 
 sed -i -E "s@${ORIGIN_URL}@${GITHUB_URL}@" README.md
 sed -i -E "s@${ORIGIN_OWNER}@${GITHUB_OWNER}@" README.md
-sed -i -E "s@${ORIGIN_AUTHOR}@${GITHUB_AUTHOR}@" README.md
+sed -i -E "s@${ORIGIN_AUTHOR_NAME}@${GITHUB_AUTHOR_NAME}@" README.md
 sed -i -E "s@${ORIGIN_REPOSITORY}@${GITHUB_REPOSITORY}@" README.md
 sed -i -E "s@${ORIGIN_DESCRIPTION}@${GITHUB_DESCRIPTION}@" README.md
 
 sed -i -E "s@${ORIGIN_URL}@${GITHUB_URL}@" book.toml
 sed -i -E "s@${ORIGIN_OWNER}@${GITHUB_OWNER}@" book.toml
-sed -i -E "s@${ORIGIN_AUTHOR}@${GITHUB_AUTHOR}@" book.toml
+sed -i -E "s@${ORIGIN_AUTHOR_NAME}@${GITHUB_AUTHOR_NAME}@" book.toml
 sed -i -E "s@${ORIGIN_REPOSITORY}@${GITHUB_REPOSITORY}@" book.toml
 sed -i -E "s@${ORIGIN_DESCRIPTION}@${GITHUB_DESCRIPTION}@" book.toml
 
 sed -i -E "s@\[yyyy\]@$(date "+%Y")@" LICENSE
-sed -i -E "s@\[name of copyright owner\]@${GITHUB_AUTHOR}@" LICENSE
+sed -i -E "s@\[name of copyright owner\]@${GITHUB_AUTHOR_NAME}@" LICENSE
