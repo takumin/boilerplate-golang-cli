d49f1a5238682f3476f4b73872b6f61cdebe4d16
Takumi Takahashi <takumiiinn@gmail.com>
2024-12-08 14:51:30 +0900

    refactor(linter-matrix): remove unused code and log needs

--- scripts/linter-matrix.cjs
+++ scripts/linter-matrix.cjs
@@ -1,7 +1,9 @@
-module.exports = ({ needs }) => {
-	var targets = [];
-	if (needs["github-actions"] === "true") {
-		targets.push("actionlint");
-	}
-	return targets;
+module.exports = (needs) => {
+	// var targets = [];
+	// if (needs["github-actions"] === "true") {
+	// 	targets.push("actionlint");
+	// }
+	// return targets;
+
+	console.log(needs);
 };
