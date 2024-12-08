a0dbbc0b95759b90173f7fee5eb8f1a7172a4063
Takumi Takahashi <takumiiinn@gmail.com>
2024-12-08 14:09:30 +0900

    refactor(linter-matrix): rename parameter and parse JSON

--- scripts/linter-matrix.cjs
+++ scripts/linter-matrix.cjs
@@ -1,4 +1,5 @@
-module.exports = ({ needs }) => {
+module.exports = ({ json }) => {
+	const needs = JSON.parse(json);
 	var targets = [];
 	if (needs["github-actions"] === "true") {
 		targets.push("actionlint");
